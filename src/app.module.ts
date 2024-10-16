3afd2ad679c673294dc7544329ccbdac350a2f4c
TrinhMinhKhoi <khoikhtn2109@gmail.com>
2024-10-17 00:48:51 +0700

    JWT

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { Task } from "./task/task.entity";
 import { UserModule } from "./user/user.module";
 import { TaskModule } from "./task/task.module";
 import { dataSourceOptions } from "db/data-source";
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(dataSourceOptions),
     UserModule,
     TaskModule,
+    AuthModule,
   ],
 })
 
